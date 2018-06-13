3b1a80bf9fd50fe1e89e4b90f7743e406173651a
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-13 14:35:34 -0400

    initial commit

---
+++ w1d3-object-drills-gist.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Akim & Mitch object drills exercises from the gist - w1d3
+//Object initializers and methods
+
+//Create an object called loaf using an object initializer ({}) 
+//with two properties: flour, which should be set to 300 and water 
+//which should be set to 210.
+let loaf = { flour: 300, water: 210 };
+//Use console.log to print the flour and water properties.
+console.log(loaf.flour + ' ' + loaf.water);
